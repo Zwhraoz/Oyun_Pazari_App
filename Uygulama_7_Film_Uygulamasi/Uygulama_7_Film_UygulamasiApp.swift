93aa13482e2eecb6952abd9ca01ea7aa2eeeb686
Zehra Özer <119448653+Zwhraoz@users.noreply.github.com>
2024-05-09 21:41:46 +0300

    Initial Commit

---
+++ Uygulama_7_Film_Uygulamasi/Uygulama_7_Film_UygulamasiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Uygulama_7_Film_UygulamasiApp.swift
+//  Uygulama_7_Film_Uygulamasi
+//
+//  Created by zehra özer on 9.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct Uygulama_7_Film_UygulamasiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
